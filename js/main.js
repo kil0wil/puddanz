51ec3274a58fb355adf9c41c9751a2e93c7a8462
DWIGHT WILLIAMS <KIL0WILL@AOL.COM>
2020-08-22 22:41:41 -0700

    edited male and female nav

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function openFemaleNav() {
+    document.getElementById("FemaleSidenav").style.width = "48%";
+}
+
+function closeFemaleNav() {
+    document.getElementById("FemaleSidenav").style.width = "0";
+}
+
+function openMaleNav() {
+    document.getElementById("MaleSidenav").style.width = "48%";
+}
+
+function closeMaleNav() {
+    document.getElementById("MaleSidenav").style.width = "0";
+}
